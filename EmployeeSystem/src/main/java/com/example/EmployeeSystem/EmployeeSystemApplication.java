57fa365c0c0fefefcb3a82f7642019ad759e5069
LeydiRiv <leydi.riv1@gmail.com>
2024-10-15 12:35:00 -0600

    Ticket#5: Microservices

--- EmployeeSystem/src/main/java/com/example/EmployeeSystem/EmployeeSystemApplication.java
+++ EmployeeSystem/src/main/java/com/example/EmployeeSystem/EmployeeSystemApplication.java
@@ -2,8 +2,10 @@ package com.example.EmployeeSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EmployeeSystemApplication {
 
 	public static void main(String[] args) {
