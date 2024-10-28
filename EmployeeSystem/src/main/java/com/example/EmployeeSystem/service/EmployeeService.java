d26c85c1e3b4d46baee92645e27abd9ddf43346c
LeydiRiv <leydi.riv1@gmail.com>
2024-10-28 11:16:44 -0600

    Ticket7: Refactor and remove code that is no longer in use

--- EmployeeSystem/src/main/java/com/example/EmployeeSystem/service/EmployeeService.java
+++ EmployeeSystem/src/main/java/com/example/EmployeeSystem/service/EmployeeService.java
@@ -9,13 +9,7 @@ import org.springframework.data.util.ReflectionUtils;
 import org.springframework.stereotype.Service;
 //import org.springframework.util.ReflectionUtils;
 
-
-import java.lang.reflect.Field;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
 
 @Service
 public class EmployeeService {
@@ -45,77 +39,19 @@ public class EmployeeService {
     }
 
 
-//    //Check-in
-//    public Employee checkIn(Long id){
-//        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Error! The Employee doesn't exist."));
-//        CheckIn checkin = new CheckIn(); //Create a new object from check-in
-//        checkin.setStatus(true);
-//        checkin.setCheckInTime(LocalDateTime.now()); //Save the dateTime
-//        checkin.setEmployee(employee); // Associate the check-in to the Employee
-//        employee.getCheckIns().add(checkin); // Save the check-in in the ArrayList
-//        return employeeRepository.save(employee);
-//
-//    }
+    public Employee updateEmployee(Long id, Employee updatedEmployee) {
+        //Check if the employee exist
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Error! The Employee doesn't exist."));
 
-
-    public Employee updateEmployee(Long id, Map<String, Object> updates) {
-        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Error! The Employee doesn't exist."));
-        // Update the specific data
-        updates.forEach((op, value) -> {
-            switch (op) {
-                case "name":
-                    employee.setName((String) value);
-                    break;
-                case "position":
-                    employee.setPosition((String) value);
-                    break;
-                case "department":
-                    employee.setDepartment((String) value);
-                    break;
-                case "email":
-                    employee.setEmail((String) value);
-                    break;
-                default:
-                    throw new RuntimeException("Error! Field " + op + " cannot be updated.");
-            }
-        });
+        // update
+        employee.setName(updatedEmployee.getName());
+        employee.setPosition(updatedEmployee.getPosition());
+        employee.setDepartment(updatedEmployee.getDepartment());
+        employee.setEmail(updatedEmployee.getEmail());
 
         return employeeRepository.save(employee);
     }
-
-
-
-
-
-
-//
-//    //Check-out
-//    public Employee checkOut(Long id) {
-//        //If the Employee exist
-//        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Error! The Employee doesn't exist."));
-//        // Is the check-in do?
-//        CheckIn checkin1 = null;
-//        for (CheckIn checkIn : employee.getCheckIns()) {
-//            if (checkIn.getStatus() != null && checkIn.getStatus()) {
-//                checkin1 = checkIn;
-//                break; // Leave into the loop
-//            }
-//        }
-//        // If the check-in isn't true, send exception
-//        if (checkin1 == null) {
-//            throw new RuntimeException("Error! Mandatory to first check in.");
-//        }
-//
-//        checkin1.setCheckOutTime(LocalDateTime.now());
-//        checkin1.setStatus(false);
-//
-//        return employeeRepository.save(employee);
-//
-//    }
-
-
-
-
 
 
     //All the records
